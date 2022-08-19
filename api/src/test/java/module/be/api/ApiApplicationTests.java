8df1a37d47926ada6c164d7941934bcc45f5b6da
Cronian <cronian89@gmail.com>
2022-08-19 15:11:49 +0900

    init project

---
+++ api/src/test/java/module/be/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package module.be.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
