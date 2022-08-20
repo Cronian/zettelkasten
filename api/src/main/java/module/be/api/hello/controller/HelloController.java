b6da2591003c1824424a74c67dbc9a36913e1041
Cronian <cronian89@gmail.com>
2022-08-20 15:21:39 +0900

    add H2 Database & JPA
    H2 데이터베이스와 JPA 추가

--- api/src/main/java/module/be/api/hello/controller/HelloController.java
+++ api/src/main/java/module/be/api/hello/controller/HelloController.java
@@ -1,4 +1,4 @@
-package module.be.api.hello;
+package module.be.api.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello would";
+        return "Hello world";
     }
 }
