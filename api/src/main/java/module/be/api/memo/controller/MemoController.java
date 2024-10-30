3684dfe724320e02b33047bd1930bd2d0bc5a57f
Cronian <cronian89@gmail.com>
2024-10-30 22:43:35 +0900

    fix get api data
    # api
    - Entity를 직접 전달하는 위험한 구조 제거 ( DTO를 통해 )
    - getter가 없어서 정상적으로 표시되지 않던 문제 해결
    - 용어 혼선을 막기위해 VO / DTO / Entity 용어정리

--- api/src/main/java/module/be/api/memo/controller/MemoController.java
+++ api/src/main/java/module/be/api/memo/controller/MemoController.java
@@ -3,7 +3,6 @@ package module.be.api.memo.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import module.be.api.memo.dto.MemoDto;
-import module.be.api.memo.entity.Memo;
 import module.be.api.memo.service.MemoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import java.util.List;
 public class MemoController {
 
     private final MemoService memoService;
-
     @GetMapping("/list")
     public List<MemoDto> list() {
         return  memoService.getAll();
